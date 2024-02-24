7358985e32213e4631aa930cbc74b83922bba0ba
vovasobol1 <dengyrostov@gmail.com>
2024-02-24 14:40:47 +0300

    рассмотрена работа сериализаторов

--- drf/drfsite/myApp/serializers.py
+++ drf/drfsite/myApp/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
-from myApp.models import People
+
+from rest_framework.renderers import JSONRenderer
+
+from .models import People
+
+# class PeopleModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
 
-class PeopleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = People
-        fields = ('title','cat_id')
+class PeopleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = PeopleModel('testPeople ', "testPeople_content")
+#     modelSR = PeopleSerializer(model)
+#     print(modelSR.data, type(modelSR.data), sep='\n')
+#     json = JSONRenderer().render(modelSR.data)
+#     print(json)
+
--- drf/drfsite/myApp/views.py
+++ drf/drfsite/myApp/views.py
@@ -1,13 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+
 from .models import People
 from .serializers import PeopleSerializer
-
+from rest_framework.views import APIView
 
 # Create your views here.
-class PeopleAPIView():
+class PeopleAPIView(APIView):
+    def get(self, request ):
+        allPeople = People.objects.all()
+        return Response({'peoples' : PeopleSerializer(allPeople , many=True).data})
+    def post(self , request):
+        serializer = PeopleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-
+        post_new = People.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post':PeopleSerializer(post_new).data})
 
 # class PeopleAPIView(generics.ListAPIView):
 #     queryset = People.objects.all()
